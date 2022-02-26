63d93cdf862fd768e588f77076f0aefa1bd6b07a
MGlolenstine <mglolenstine@gmail.com>
2022-02-26 23:26:17 +0100

    Removed lease time, as it's not needed anymore. Now ports are open as long as the app is open.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use clipboard_ext::prelude::ClipboardProvider;
 use gtk::{
     prelude::{ComboBoxExtManual, WidgetExt},
     traits::{BoxExt, ButtonExt, ComboBoxExt, EntryExt, GridExt, GtkWindowExt, OrientableExt},
-    Align, EntryBuffer,
+    Align, EntryBuffer, Inhibit,
 };
 use mc_server_scanner::packets::ServerQueryResponse;
 use native_dialog::{MessageDialog, MessageType};
@@ -18,9 +18,7 @@ use utils::{
     port_forwarding::redirect_minecraft_to_a_port,
 };
 
-#[cfg(not(unix))]
-use portforwarder_rs::port_forwarder::Forwarder;
-use std::sync::{Arc, Mutex};
+use portforwarder_rs::port_forwarder::{Forwarder, PortMappingProtocol};
 
 pub mod utils;
 struct AppComponents {
@@ -41,32 +39,28 @@ enum AppMsg {
     ServerScanResults(Vec<(ServerQueryResponse, u16)>),
     SelectedPort(Option<u16>),
     PortForward,
+    Close,
 }
 
 #[tracker::track]
 struct AppModel {
     public_ip: IpAddr,
     private_ip: IpAddr,
-    lease_time: u32,
     external_port: u16,
     internal_ports: Vec<(String, u16)>,
     selected_minecraft_port: Option<u16>,
     scanning: bool,
-    #[cfg(not(unix))]
     forwarder: MyForwarder,
 }
 
-#[cfg(not(unix))]
 pub struct MyForwarder(Forwarder);
 
-#[cfg(not(unix))]
 impl PartialEq for MyForwarder {
     fn eq(&self, other: &Self) -> bool {
         self.0.network_interface == other.0.network_interface
     }
 }
 
-#[cfg(not(unix))]
 impl Eq for MyForwarder {}
 
 impl Model for AppModel {
@@ -101,72 +95,50 @@ impl AppUpdate for AppModel {
                 self.reset();
             }
             AppMsg::PortForward => {
-                if let IpAddr::V4(local_addr) = self.get_private_ip() {
-                    let local_addr = local_addr.clone();
-                    if let Some(selected_minecraft_port) = self.get_selected_minecraft_port() {
-                        let selected_minecraft_port = selected_minecraft_port.clone();
-                        #[cfg(not(unix))]
-                        let redirect = {
-                            let ports = *self.get_external_port();
-                            let lease = *self.get_lease_time();
-                            let MyForwarder(fwd) = self.get_mut_forwarder();
-                            redirect_minecraft_to_a_port(
-                                #[cfg(not(unix))]
-                                fwd,
-                                local_addr,
-                                selected_minecraft_port,
-                                ports,
-                                lease,
-                            )
-                        };
-                        #[cfg(unix)]
-                        let redirect = {
-                            redirect_minecraft_to_a_port(
-                                local_addr,
-                                *selected_minecraft_port,
-                                *self.get_external_port(),
-                                *self.get_lease_time(),
-                            )
-                        };
-                        if let Err(a) = redirect {
-                            MessageDialog::new()
-                                .set_type(MessageType::Error)
-                                .set_title("Error")
-                                .set_text(&format!("Error occured while opening the ports.\n{}", a))
-                                .show_alert()
-                                .unwrap();
-                            error!("{:?}", a);
-                        } else {
-                            let mut ctx = ClipboardContext::new().unwrap();
-                            ctx.set_contents(format!(
-                                "{}:{}",
-                                self.get_public_ip(),
-                                self.get_external_port()
-                            ))
-                            .unwrap();
-                            MessageDialog::new()
+                if let Some(selected_minecraft_port) = self.get_selected_minecraft_port() {
+                    let selected_minecraft_port = *selected_minecraft_port;
+                    let redirect = {
+                        let ports = *self.get_external_port();
+                        let MyForwarder(fwd) = self.get_mut_forwarder();
+                        redirect_minecraft_to_a_port(fwd, selected_minecraft_port, ports)
+                    };
+                    if let Err(a) = redirect {
+                        show_error(&format!("Error occured while opening the ports.\n{}", a));
+                        error!("{:?}", a);
+                    } else {
+                        let mut ctx = ClipboardContext::new().unwrap();
+                        ctx.set_contents(format!(
+                            "{}:{}",
+                            self.get_public_ip(),
+                            self.get_external_port()
+                        ))
+                        .unwrap();
+                        MessageDialog::new()
                                 .set_type(MessageType::Info)
                                 .set_title("Port opened!")
-                                .set_text(&format!("Minecraft should now be redirected from port {} to {} with lease time of {}s.\nPeople can join using {}:{}, it has also been copied to your clipboard.", selected_minecraft_port, self.get_external_port(), self.get_lease_time(), self.get_public_ip(), self.get_external_port()))
+                                .set_text(&format!("Minecraft should now be redirected from port {} to {}.\nPort will be open so long until the app is closed.\nPeople can join using {}:{}, it has also been copied to your clipboard.", selected_minecraft_port, self.get_external_port(), self.get_public_ip(), self.get_external_port()))
                                 .show_alert()
                                 .unwrap();
-                        }
-                    } else {
-                        MessageDialog::new()
-                            .set_type(MessageType::Error)
-                            .set_title("Error")
-                            .set_text("No minecraft server selected.")
-                            .show_alert()
-                            .unwrap();
                     }
                 } else {
-                    MessageDialog::new()
-                            .set_type(MessageType::Error)
-                            .set_title("Error")
-                            .set_text("Your local IP doesn't seem like it's an IPv4.\nIPv6 isn't supported in IGD yet.")
-                            .show_alert()
-                            .unwrap();
+                    show_error("No minecraft server selected.");
                 }
+            }
+            AppMsg::Close => {
+                let ports = self
+                    .forwarder
+                    .0
+                    .open_ports
+                    .iter()
+                    .map(|a| (a.num, a.proto))
+                    .collect::<Vec<(u16, PortMappingProtocol)>>();
+                for port in ports.iter() {
+                    if let Err(e) = self.forwarder.0.remove_port(port.0, port.1) {
+                        error!("Failed to close port {}/{} due to {}", port.0, port.1, e);
+                    }
+                }
+                debug!("All ports closed, closing the application.");
+                return false;
             }
         }
         true
@@ -177,6 +149,10 @@ impl AppUpdate for AppModel {
 impl Widgets<AppModel, ()> for AppWidgets {
     view! {
         main_window = gtk::ApplicationWindow {
+            connect_close_request(sender) => move |_|{
+                send!(sender, AppMsg::Close);
+                Inhibit(false)
+            },
             set_child = Some(&gtk::Box){
                 set_margin_all: 5,
                 set_orientation: gtk::Orientation::Vertical,
@@ -193,13 +169,9 @@ impl Widgets<AppModel, ()> for AppWidgets {
                     },
                     attach(0, 2, 1, 1) = &gtk::Label{
                         set_halign: Align::Start,
-                        set_label: "Lease time"
-                    },
-                    attach(0, 3, 1, 1) = &gtk::Label{
-                        set_halign: Align::Start,
                         set_label: "External port"
                     },
-                    attach(0, 4, 1, 1) = &gtk::Label{
+                    attach(0, 3, 1, 1) = &gtk::Label{
                         set_halign: Align::Start,
                         set_label: "Internal port"
                     },
@@ -212,36 +184,29 @@ impl Widgets<AppModel, ()> for AppWidgets {
                         set_halign: Align::Start,
                         set_label: watch!(&model.get_private_ip().to_string())
                     },
-                    attach(1, 2, 1, 1) = &gtk::SpinButton::with_range(1.0, 100000.0, 1.0){
-                        set_value: watch!(*model.get_lease_time() as f64)
-                    },
-                    attach(1, 3, 1, 1) = &gtk::Entry{
+                    attach(1, 2, 1, 1) = &gtk::Entry{
                         set_buffer: watch!(&EntryBuffer::new(Some(&format!("{}", model.get_external_port())))),
                     },
-                    attach(1, 4, 1, 1): internal_ports = &gtk::ComboBoxText{
+                    attach(1, 3, 1, 1): internal_ports = &gtk::ComboBoxText{
                         set_hexpand: true,
                         connect_changed(sender) => move |a|{
-                            let port = if let Some(id) = a.active_id(){
-                                Some(id.to_string().parse::<u16>().unwrap())
-                            }else{
-                                None
-                            };
+                            let port = a.active_id().map(|id| id.to_string().parse::<u16>().unwrap());
                             send!(sender, AppMsg::SelectedPort(port));
                         }
                     },
                     //? Buttons
-                    attach(0, 5, 1, 1) = &gtk::Button{
+                    attach(0, 4, 1, 1) = &gtk::Button{
                         set_label: "Rescan minecraft instances",
                         set_visible: watch!(!model.scanning),
                         connect_clicked(sender) => move |_|{
                             send!(sender, AppMsg::RescanServers);
                         }
                     },
-                     attach(0, 5, 1, 1) = &gtk::Spinner{
+                    attach(0, 4, 1, 1) = &gtk::Spinner{
                         set_spinning: true,
                         set_visible: watch!(model.scanning),
                     },
-                    attach(1, 5, 1, 1) = &gtk::Button{
+                    attach(1, 4, 1, 1) = &gtk::Button{
                         set_label: "Start forwarding",
                         connect_clicked(sender) => move |_|{
                             send!(sender, AppMsg::PortForward);
@@ -268,6 +233,15 @@ impl Widgets<AppModel, ()> for AppWidgets {
     }
 }
 
+fn show_error(text: &str) {
+    MessageDialog::new()
+        .set_type(MessageType::Error)
+        .set_title("Error")
+        .set_text(text)
+        .show_alert()
+        .unwrap();
+}
+
 fn main() {
     #[cfg(windows)]
     let _ = ansi_term::enable_ansi_support();
@@ -283,7 +257,6 @@ fn main() {
     };
     if let Some(pub_addr) = pub_addr {
         if let Some(local_addr) = get_local_ip() {
-            #[cfg(not(unix))]
             let forwarder = if let IpAddr::V4(a) = local_addr {
                 if let Ok(a) = portforwarder_rs::port_forwarder::create_forwarder(a) {
                     a
@@ -298,31 +271,21 @@ fn main() {
             let model = AppModel {
                 public_ip: pub_addr,
                 private_ip: local_addr,
-                lease_time: 3600,
                 external_port: 25565,
                 internal_ports: vec![],
                 selected_minecraft_port: None,
                 scanning: false,
-                #[cfg(not(unix))]
                 forwarder: MyForwarder(forwarder),
                 tracker: 0,
             };
             let relm = RelmApp::new(model);
             relm.run();
         } else {
-            MessageDialog::new()
-                .set_type(MessageType::Error)
-                .set_title("Error")
-                .set_text("There was an error retrieving your local IP.")
-                .show_alert()
-                .unwrap();
+            show_error(
+                "Your local IP doesn't seem like it's an IPv4.\nIPv6 isn't supported in IGD yet.",
+            );
         }
     } else {
-        MessageDialog::new()
-            .set_type(MessageType::Error)
-            .set_title("Error")
-            .set_text("There was an error retrieving your public IP.")
-            .show_alert()
-            .unwrap();
+        show_error("There was an error retrieving your public IP.");
     }
 }
--- src/utils/port_forwarding.rs
+++ src/utils/port_forwarding.rs
@@ -1,60 +1,20 @@
-use std::net::Ipv4Addr;
-
 use anyhow::{bail, Result};
 use tracing::debug;
 
-/// Uses UPnP to port-forward the automagically generated port to the defined external one.
-#[cfg(unix)]
-pub fn redirect_minecraft_to_a_port(
-    local_addr: Ipv4Addr,
-    mc_port: u16,
-    wanted_port: u16,
-    lease: u32,
-) -> Result<()> {
-    let local_addr = SocketAddrV4::new(local_addr, mc_port);
-    match igd::search_gateway(Default::default()) {
-        Err(ref err) => bail!("Error finding gateway: {}", err),
-        Ok(gateway) => {
-            match gateway.add_port(
-                igd::PortMappingProtocol::TCP,
-                wanted_port,
-                local_addr,
-                lease,
-                "MinecraftLAN",
-            ) {
-                Err(ref err) => {
-                    bail!("There was an error registering the port! {}", err);
-                }
-                Ok(_) => {
-                    debug!("It worked! Got port {}, hopefully!", wanted_port);
-                }
-            }
-        }
-    }
-    Ok(())
-}
-
-#[cfg(not(unix))]
 use portforwarder_rs::port_forwarder::Forwarder;
 
-#[cfg(not(unix))]
 pub fn redirect_minecraft_to_a_port(
     forwarder: &mut Forwarder,
-    local_addr: Ipv4Addr,
     mc_port: u16,
     wanted_port: u16,
-    _lease: u32,
 ) -> Result<()> {
-    if forwarder
-        .forward_port(
-            mc_port,
-            wanted_port,
-            portforwarder_rs::port_forwarder::PortMappingProtocol::TCP,
-            "MinecraftLAN",
-        )
-        .is_err()
-    {
-        bail!("Failed to open the port!");
+    if let Err(e) = forwarder.forward_port(
+        mc_port,
+        wanted_port,
+        portforwarder_rs::port_forwarder::PortMappingProtocol::TCP,
+        "MinecraftLAN",
+    ) {
+        bail!("Failed to open the port!\n{}", e);
     }
     debug!("It worked! Got port {}, hopefully!", wanted_port);
     Ok(())
